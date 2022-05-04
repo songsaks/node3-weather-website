c257b95bb47a55940b0d4daf4d1a1775252aadd0
songsaks <aeekae@gmail.com>
2022-05-04 16:43:16 +0700

    Add humidity forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -29,7 +29,8 @@ const forecast = (latitude,longitude, callback) =>{
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degree out.')
+            console.log()
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degree out. The humidity is ' + body.current.humidity + "%.")
         }
     })
 }
